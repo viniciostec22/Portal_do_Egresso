ea36aab8950101cd1202e716709e2b4e9804fcc5
VINICIOS MATHEUS OLIVEIRA DA SILVA <106559003+viniciostec22@users.noreply.github.com>
2023-03-24 10:35:58 -0300

    filtro_depoimentos
    url depoimentos e container de filtragem dos depoimentos

--- Home/views.py
+++ Home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def Home(request):
   return render(request, 'home.html')
+
+def Depoimentos(request):
+  return render(request, 'depoimentos.html')
