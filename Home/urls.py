16db879c11180f6cb32edab417591f44e297e104
VINICIOS MATHEUS OLIVEIRA DA SILVA <106559003+viniciostec22@users.noreply.github.com>
2023-03-27 11:44:44 -0300

    active_navbar

--- Home/urls.py
+++ Home/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.Home, name='home'),
     path('depoimentos/', views.Depoimentos, name='depoimentos'),
     path('objetivos/', views.objetivos, name='objetivos'),
-    path('empregos-carreira', views.empregos_carrera, name='empregos-carreira')
+    path('empregos-carreira', views.empregos_carrera, name='empregos_carreira')
 ]
