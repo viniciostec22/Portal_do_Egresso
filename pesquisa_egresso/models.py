5d960edaeaf5a259bf4b293578927758425bd41e
VINICIOS MATHEUS OLIVEIRA DA SILVA <106559003+viniciostec22@users.noreply.github.com>
2023-05-15 21:13:24 -0300

    stylisação form

--- pesquisa_egresso/models.py
+++ pesquisa_egresso/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
+from Home.models import Endereco
+
 class Pesquisa(models.Model):
     nome_completo = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20)
-    endereco = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=200, verbose_name='Endereço')
     email = models.EmailField()
 
     curso = models.CharField(max_length=100)
-    ano_conclusao = models.PositiveIntegerField()
+    ano_conclusao = models.PositiveIntegerField(verbose_name='Ano de Conclusão')
     sexo = models.CharField(max_length=10)
     idade = models.PositiveIntegerField()
     estado_civil = models.CharField(max_length=20)
@@ -17,29 +19,29 @@ class Pesquisa(models.Model):
 
     motivo_escolha_curso = models.TextField()
     expectativas_curso = models.TextField()
-    avaliacao_aprendizado = models.PositiveIntegerField()
-    avaliacao_curso = models.PositiveIntegerField()
-    participacao_pesquisa = models.BooleanField()
-    participacao_extensao = models.BooleanField()
+    avaliacao_aprendizado = models.PositiveIntegerField(verbose_name='Avaliação Aprendizado')
+    avaliacao_curso = models.PositiveIntegerField(verbose_name='Avaliação Curso')
+    participacao_pesquisa = models.BooleanField( verbose_name='Participação Projeto de Pesquisa')
+    participacao_extensao = models.BooleanField(verbose_name='Participação Projeto de Extenção')
     monitor = models.BooleanField()
-    realizacao_estagios = models.BooleanField()
-    contribuicao_monitor_estagio = models.TextField()
+    realizacao_estagios = models.BooleanField(verbose_name='Realização de Estagio')
+    contribuicao_monitor_estagio = models.TextField(verbose_name='Contribuição monitor estagio')
 
     experiencia_trabalho = models.TextField()
-    trabalho_area_formacao = models.CharField(max_length=100)
+    trabalho_area_formacao = models.CharField(max_length=100,verbose_name='Trabalho Aréa Formação')
     situacao_atual = models.CharField(max_length=50)
     meio_conseguir_trabalho = models.TextField()
     tipo_vinculo = models.CharField(max_length=50)
 
-    instituicao = models.CharField(max_length=100)
+    instituicao = models.CharField(max_length=100, verbose_name='Instituição')
     nome_curso = models.CharField(max_length=100)
 
     contribuicoes_if = models.TextField()
     dificuldades_curso = models.TextField()
-    experiencias_if = models.TextField()
+    experiencias_if = models.TextField(verbose_name='Experiencias IF')
 
-    sugestoes_relacao = models.TextField()
-    sugestoes_homepage = models.TextField()
+    sugestoes_relacao = models.TextField(verbose_name='Sugestões relação')
+    sugestoes_homepage = models.TextField(verbose_name='Sugestões homepage')
     
     def __str__(self):
         return self.nome_completo
